74e9bae2991030ec27191dc3b3eee20418be5e32
Oskar Gawlik <o.gav13733@gmail.com>
2022-10-17 21:45:59 +0200

    better version of app with DRY, re-usability, hashbang

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
-with open('text.txt', 'r') as file:
-    for line in file.readlines():
-        for word in line.split():
-            print(word)
+
+
+import sys
+
+
+def fetch_words(filepath):
+    with open(filepath, 'r') as file:
+        return [word for line in file for word in line.split()]
+
+
+def print_items(items):
+    for item in items:
+        print(item)
+
+
+def main(file):
+    words = fetch_words(file)
+    print_items(words)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
