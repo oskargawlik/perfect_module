683503e40484420dd9c76fbd31be4ac0446c141e
Oskar Gawlik <o.gav13733@gmail.com>
2022-10-17 22:27:40 +0200

    documantation using docstring

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
 
 
+"""Fetch and print words
+
+Usage:
+    python main.py <filepath>
+    ./main.py <filepath>
+    from main import main
+    main(<filepath)
+
+"""
+
 import sys
 
 
 def fetch_words(filepath):
+    """
+    Fetch words from given filepath
+    :param filepath: relative or absolute filepath
+    :type filepath: str
+    :return: word list from given filepath
+    :rtype: list[str]
+    """
     with open(filepath, 'r') as file:
         return [word for line in file for word in line.split()]
 
 
 def print_items(items):
+    """
+    Prints items from iterable
+    :param items: any iterable collection of data
+    :type items: iterable
+    :return: none
+    :rtype: none
+    """
     for item in items:
         print(item)
 
 
-def main(file):
-    words = fetch_words(file)
+def main(filepath):
+    """
+    Fetch words from given filepath
+    :param filepath: relative or absolute filepath
+    :type filepath: str
+    :return: None
+    :rtype: None
+    """
+    words = fetch_words(filepath)
     print_items(words)
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1])  # Zero-index is module name
